1c6f820edc01fe51a6e9ac1649a828b05f3e0a39
SyAthar1990 <sbinamir@gmail.com>
2016-08-11 16:30:20 +0600

    map unbootstrapped

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function initialize(){
+  var mapOptions ={
+    center: new google.maps.LatLng(23.0,90.0),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.HYBRID,
+    scrollwheel: false,
+    draggable: true,
+    panControl: true,
+    zoomControl: true,
+    mapTypeControl: true,
+    scaleControl: true,
+    streetViewControl: true,
+    overviewMapControl: true,
+    rotateControl: true,
+  };
+
+var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+}
+google.maps.event.addDomListener(window,'load',initialize);
