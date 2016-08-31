63384f93d1cb43a573565e59ff9296e58e0f1ecd
zymaislam <zyma.islam90@gmail.com>
2016-08-31 14:40:48 +0600

    fixed slider positioningand properties

--- js/script.js
+++ js/script.js
@@ -18,3 +18,21 @@ function initialize(){
 var map = new google.maps.Map(document.getElementById("map"),mapOptions);
 }
 google.maps.event.addDomListener(window,'load',initialize);
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+}
